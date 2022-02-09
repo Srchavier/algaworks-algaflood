b905ec6c471775aac1b00afd48188ab1a2cef9e0
Eduardo R Fernandes <duduhyx@gmail.com>
2022-02-09 23:43:06 +0000

    first commit

---
+++ src/main/java/com/algaworks/algafloodapi/AlgafloodApiApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafloodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafloodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgafloodApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algaworks/algafloodapi/AlgafloodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafloodapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafloodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
